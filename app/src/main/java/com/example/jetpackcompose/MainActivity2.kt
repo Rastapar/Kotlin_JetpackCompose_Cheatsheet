4d960590df06c404709f9c982b032459dcad04be
rastapar <romancher799@gmail.com>
2023-10-11 19:26:27 +0200

    Until Round Timer

--- app/src/main/java/com/example/jetpackcompose/MainActivity2.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity2.kt
@@ -44,6 +44,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Snackbar
@@ -63,7 +64,9 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.painter.Painter
@@ -72,6 +75,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.boundsInWindow
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
@@ -85,6 +89,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -100,7 +105,9 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.math.PI
 import kotlin.math.atan2
+import kotlin.math.cos
 import kotlin.math.roundToInt
+import kotlin.math.sin
 import kotlin.random.Random
 
 /*
@@ -128,7 +135,8 @@ class MainActivity2 : ComponentActivity() {
 //            EffectHandlers()    // Just theory
 //            SimpleAnimations()
 //            CircularProgressBar()
-            DraggableMusicKnob()
+//            DraggableMusicKnob()
+            RoundTimer()
         }
     }
 }
@@ -840,6 +848,138 @@ fun DraggableMusicBarComponent (
                 )
 
             }
+        }
+    }
+}
+
+@Composable
+fun RoundTimer(){
+    val secondsTimer = 10L
+    Surface (
+        color = Color(0xFF101010),
+        modifier = Modifier.fillMaxSize()
+    ){
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+            RoundTimerComponent(
+                totalTime = secondsTimer * 1000,
+                handleColor = Color.Green,
+                inactiveBarColor = Color.DarkGray,
+                activeBarColor = Color(0xFF37B900),
+                modifier = Modifier.size(200.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun RoundTimerComponent (
+    totalTime: Long,
+    handleColor: Color,
+    inactiveBarColor: Color,
+    activeBarColor: Color,
+    modifier: Modifier,
+    initialValue: Float = 1f,
+    strokeWidth: Dp = 5.dp
+) {
+    var size by remember {
+        mutableStateOf(IntSize.Zero)
+    }
+    var value by remember {
+        mutableFloatStateOf(initialValue)
+    }
+    var currentTime by remember {
+        mutableLongStateOf(totalTime)
+    }
+    var isTimeRunning by remember {
+        mutableStateOf(false)
+    }
+    /*
+    When the user presses the start button, 'isTimeRunning' will become true and as some value
+    changed, the block will be executed.
+    After the timer is on, the 'currentTime' will be counting down and as the value itself will
+    be changing then 'LaunchedEffect' will be calling itself until the timer ends.
+    We subtract 100 milliseconds from the 'currentTime' to compensate the delay
+    */
+    LaunchedEffect(key1 = currentTime, key2 = isTimeRunning) {
+        if (currentTime > 0 && isTimeRunning) {
+            delay(100)
+            currentTime -= 100
+            value = currentTime / totalTime.toFloat()
+        }
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .onSizeChanged {
+                size = it
+            }
+    ) {
+        Canvas(
+            modifier = modifier
+        ) {
+            drawArc(
+                color = inactiveBarColor,
+                startAngle = -215f,
+                sweepAngle = 250f,
+                useCenter = false,
+                topLeft = Offset.Zero,
+                size = Size(size.width.toFloat(), size.height.toFloat()),
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+            drawArc(
+                color = activeBarColor,
+                startAngle = -215f,
+                sweepAngle = 250f * value,
+                useCenter = false,
+                topLeft = Offset.Zero,
+                size = Size(size.width.toFloat(), size.height.toFloat()),
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+            val center = Offset(size.width / 2f, size.height / 2f)
+            val beta = (250f * value + 145f) * (PI / 180f).toFloat()
+            val r = size.width / 2f
+            val a = cos(beta) * r
+            val b = sin(beta) * r
+            drawPoints(
+                listOf(Offset(center.x + a, center.y + b)),
+                pointMode = PointMode.Points,
+                color = handleColor,
+                strokeWidth = (strokeWidth * 3f).toPx(),
+                cap = StrokeCap.Round
+            )
+        }
+        Text(
+            text = (currentTime / 1000).toString(),
+            fontSize = 44.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Button(
+            onClick = {
+                if(currentTime <= 0) {
+                    currentTime = totalTime
+                    isTimeRunning = true
+                } else {
+                    isTimeRunning = !isTimeRunning
+                }
+            },
+            modifier = Modifier
+                .align(Alignment.BottomCenter),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = if(!isTimeRunning || currentTime <= 0) {
+                    Color.Green
+                } else {
+                    Color.Red
+                }
+            )
+        ) {
+            Text(
+                text = if (isTimeRunning && currentTime > 0) "Stop"
+                        else if(!isTimeRunning && currentTime > 0) "Start"
+                        else "Restart"
+            )
         }
     }
 }
